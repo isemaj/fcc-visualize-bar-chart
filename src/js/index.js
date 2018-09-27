c3598f706ff8973583e87af7bd0a3048b0d52c59
James Itum <jamesitum@protonmail.ch>
2018-09-27 23:40:20 +0800

    trying to make it work

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,6 @@
-import * as d3 from 'd3';
-
-import '../styles/app.scss';
-
-const api = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-
-let svg = d3.select('#chart')
-  .append('svg')
-  .attr('width', 500)
-  .attr('height', 900);
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  fetch(api)
-    .then(res => res.json())
-    .then(data => {
-
-    })
-});
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 
 import App from './components/App';
-import rootReducer from './reducers';
-import timerMiddleware from './middlewares/timerMiddleware';
 
-const middleware = [logger, timerMiddleware];
-const middlewareEnhancer = applyMiddleware(...middleware);
-
-const enhancers = [middlewareEnhancer];
-const composedEnhancers = composeWithDevTools(...enhancers);
-
-const store = createStore(rootReducer, {}, composedEnhancers);
-
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
