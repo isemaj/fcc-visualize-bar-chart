db521626ff6ca3de6e2d9081f7434301238c4028
James Itum <jamesitum@protonmail.ch>
2018-09-27 21:53:24 +0800

    confused

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log('test');
+import * as d3 from 'd3';
+
+import '../styles/app.scss';
+
+const api = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+let svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', 500)
+  .attr('height', 900);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetch(api)
+    .then(res => res.json())
+    .then(data => {
+
+    })
+});
+
