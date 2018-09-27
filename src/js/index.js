b3b2cf669b2c0ac0aaec04721f3045656c473b6a
James Itum <jamesitum@protonmail.ch>
2018-09-27 23:36:59 +0800

    upates

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     })
 });
 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+import App from './components/App';
+import rootReducer from './reducers';
+import timerMiddleware from './middlewares/timerMiddleware';
+
+const middleware = [logger, timerMiddleware];
+const middlewareEnhancer = applyMiddleware(...middleware);
+
+const enhancers = [middlewareEnhancer];
+const composedEnhancers = composeWithDevTools(...enhancers);
+
+const store = createStore(rootReducer, {}, composedEnhancers);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'),
+);
